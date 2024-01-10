0a617ac5deeeeab263051800260e00d07d075d6f
chenyaqi <henchenyaqi319@gmail.com>
2024-01-10 22:23:12 +0800

    feat: 渲染component流程梳理

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,46 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  }
+
+  return component
+}
+
+export function setupComponent(instance) {
+  // initProps()
+  // initSlots()
+
+  setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent(instance) {
+  const Component = instance.type
+
+  const { setup } = Component
+
+  if (setup) {
+    // setup可以返回一个对象或者渲染函数
+    const setupResult = setup()
+
+    handleSetupResult(instance, setupResult)
+  }
+}
+
+function handleSetupResult(instance, setupResult) {
+  // TODO function
+
+  if (typeof setupResult === "object") {
+    instance.setupState = setupResult
+  }
+
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance) {
+  const Component = instance.type
+
+  if (!Component.render) {
+    instance.render = Component.render
+  }
+}
